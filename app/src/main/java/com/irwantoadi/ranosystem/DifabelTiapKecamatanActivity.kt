023bebcc822cdd2d7cbf07c3667bd91e50dace08
irwantoadi <irwantoadi926@gmail.com>
2023-01-16 20:24:48 +0700

    project Ranosystem

---
+++ app/src/main/java/com/irwantoadi/ranosystem/DifabelTiapKecamatanActivity.kt
@@ -0,0 +1,11 @@
+package com.irwantoadi.ranosystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DifabelTiapKecamatanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_difabel_tiap_kecamatan)
+    }
+}
